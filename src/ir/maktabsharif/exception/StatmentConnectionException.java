e071efaf48f0038af619a2fe43cd87fb2b61f194
abolfazl <abolfazlovich@gmail.com>
2026-07-14 02:07:32 +0330

    feat: add StatmentConnectionException

---
+++ src/ir/maktabsharif/exception/StatmentConnectionException.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.exception;
+
+public class StatmentConnectionException extends RuntimeException {
+    public StatmentConnectionException(String message) {
+        super(message);
+    }
+}
