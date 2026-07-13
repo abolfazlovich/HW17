ec0a54ee841cc9af9eb91fab26fe0c08baa2fb49
abolfazl <abolfazlovich@gmail.com>
2026-07-13 20:04:39 +0330

    feat: add DatabaseConnection

---
+++ src/ir/maktabsharif/util/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package ir.maktabsharif.util;
+
+import ir.maktabsharif.exception.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public static final String URL = "jdbc:postgresql://localhost:5432/hw17";
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "1376";
+    public Connection getConnection(){
+        try(Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD)){
+            return connection;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+}
