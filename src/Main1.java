2e371833cd55cb19e09bd62989e29766e4ad40a0
abolfazl <abolfazlovich@gmail.com>
2026-07-14 15:43:14 +0330

    fix: Main

--- src/Main1.java
+++ src/Main1.java
@@ -1,32 +1,73 @@
+import ir.maktabsharif.enums.EventStatus;
+import ir.maktabsharif.enums.ReservationStatus;
+import ir.maktabsharif.model.Event;
+import ir.maktabsharif.model.Reservation;
+import ir.maktabsharif.service.EventService;
+import ir.maktabsharif.service.ReservationService;
+
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true){
+        EventService eventService = new EventService();
+        ReservationService reservationService = new ReservationService();
+        while (true) {
             menu();
             int operator = scanner.nextInt();
-            switch (operator){
+            switch (operator) {
                 case 1:
-
+                    System.out.println("Enter Title");
+                    String title = scanner.next();
+                    System.out.println("Enter Location");
+                    String location = scanner.next();
+                    System.out.println("Enter Capacity");
+                    Integer capacity = scanner.nextInt();
+                    System.out.println("Enter Ticket Price : $");
+                    Double ticketPrice = scanner.nextDouble();
+                    Event event = new Event(title, location, capacity, 0, ticketPrice, EventStatus.ACTIVE);
+                    eventService.create(event);
                     break;
                 case 2:
-
+                    eventService.showAll();
                     break;
                 case 3:
-
+                    System.out.println("Enter Event ID");
+                    Long updateId = scanner.nextLong();
+                    System.out.println("Enter New Title");
+                    String newTitle = scanner.next();
+                    System.out.println("Enter New Location");
+                    String newLocation = scanner.next();
+                    System.out.println("Enter New Capacity");
+                    Integer newCapacity = scanner.nextInt();
+                    System.out.println("Enter New Ticket Price : $");
+                    Double newTicketPrice = scanner.nextDouble();
+                    Event newEvent = new Event(newTitle, newLocation, newCapacity, 0, newTicketPrice, EventStatus.ACTIVE);
+                    eventService.update(newEvent, updateId);
                     break;
                 case 4:
-
+                    System.out.println("Enter Event Id");
+                    eventService.cancel(scanner.nextLong());
                     break;
                 case 5:
-
+                    System.out.println("Enter Customer Name");
+                    String customerName = scanner.next();
+                    System.out.println("Enter Customer Phone");
+                    String customerPhone = scanner.next();
+                    System.out.println("Enter Event ID");
+                    Long eventId = scanner.nextLong();
+                    System.out.println("Enter Ticket Count");
+                    Integer ticketCount = scanner.nextInt();
+                    Reservation reservation = new Reservation(customerName, customerPhone, eventId, ticketCount, LocalDate.now(), ReservationStatus.ACTIVE);
+                    reservationService.create(reservation);
                     break;
                 case 6:
-
+                    System.out.println("Enter Reservation ID");
+                    reservationService.cancel(scanner.nextLong());
                     break;
                 case 7:
-
+                    reservationService.showAll();
                     break;
                 case 8:
 
@@ -41,7 +82,8 @@ public class Main1 {
         }
 
     }
-    public static void menu(){
+
+    public static void menu() {
         System.out.println("---MENU---");
         System.out.println("1. Create Event");
         System.out.println("2. Show All Event");
