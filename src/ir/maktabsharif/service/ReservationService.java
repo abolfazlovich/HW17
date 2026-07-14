f54cf874685530caff554ba40ff070e660376c0b
abolfazl <abolfazlovich@gmail.com>
2026-07-14 14:24:51 +0330

    feat: ReservationService

---
+++ src/ir/maktabsharif/service/ReservationService.java
@@ -0,0 +1,46 @@
+package ir.maktabsharif.service;
+
+import ir.maktabsharif.exception.CapacityExceededException;
+import ir.maktabsharif.exception.EventCancelledException;
+import ir.maktabsharif.model.Event;
+import ir.maktabsharif.model.Reservation;
+import ir.maktabsharif.repository.impl.EventRepositoryImpl;
+import ir.maktabsharif.repository.impl.ReservationRepositoryImpl;
+
+import java.util.List;
+
+public class ReservationService {
+    ReservationRepositoryImpl reservationRepository = new ReservationRepositoryImpl();
+    EventRepositoryImpl eventRepository = new EventRepositoryImpl();
+
+    public void create(Reservation reservation) {
+        Event event = eventRepository.findById(reservation.getEventId());
+        if (event.getStatus().toString().equalsIgnoreCase("canceled")) {
+            throw new EventCancelledException("Event Cancelled Exception");
+        }
+        if (event.getCapacity() <= reservation.getTicketCount()) {
+            throw new CapacityExceededException("Capacity Exceeded Exception");
+        }
+        reservation.setTicketCount(reservation.getTicketCount() + 1);
+        event.setReservedCount(event.getReservedCount() + 1);
+        reservationRepository.save(reservation);
+        eventRepository.update(event, reservation.getEventId());
+    }
+
+    public void showAll() {
+List<Reservation> reservationList = reservationRepository.findAll();
+        System.out.println(reservationList);
+    }
+
+    public void update(Reservation reservation, Long id) {
+reservationRepository.update(reservation,id);
+    }
+
+    public void cancel(Long id) {
+Reservation reservation = reservationRepository.findById(id);
+Event event =eventRepository.findById(reservation.getEventId());
+reservationRepository.delete(id);
+event.setReservedCount(event.getReservedCount()-1);
+eventRepository.update(event, reservation.getEventId());
+    }
+}
